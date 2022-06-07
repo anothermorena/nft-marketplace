6d4b393237f8bd6979028b927dee07ba0b4717d4
Morena <onalepeloo@live.com>
2022-06-07 18:57:46 +0200

    implemented the handle search function

--- screens/Home.js
+++ screens/Home.js
@@ -6,6 +6,25 @@ import {COLORS, NFTData} from "../constants";
 import { HomeHeader,NFTCard, FocusedStatusBar } from '../components';
 
 const Home = () => {
+
+    const [nftData, setNftData] = useState(NFTData);
+
+    const handleSearch = value => {
+      if (value.length === 0) {
+        setNftData(NFTData);
+      }
+  
+      const filteredData = NFTData.filter((item) =>
+        item.name.toLowerCase().includes(value.toLowerCase())
+      );
+  
+      if (filteredData.length === 0) {
+        setNftData(NFTData);
+      } else {
+        setNftData(filteredData);
+      }
+    };
+
   return (
     <SafeAreaView style={{flex:1}}>
         <FocusedStatusBar background={COLORS.primary}/>
@@ -13,11 +32,11 @@ const Home = () => {
         <View style={{flex: 1}}>
             <View style={{zIndex: 0}}>
                 <FlatList 
-                    data={NFTData}
+                    data={nftData}
                     renderItem={({item}) => <NFTCard data={item}/>}
                     keyExtractor={item => item.id}
                     showsVerticalScrollIndicator={false}
-                    ListHeaderComponent={<HomeHeader/>}
+                    ListHeaderComponent={<HomeHeader onSearch={handleSearch} />}
                 />
             </View>
 
