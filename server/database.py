d22e720afdbeeb98a6ef4380a65a681b1907685d
morenamt <onalepeloo@live.com>
2022-07-12 13:24:39 +0200

    styled and properly documented the database.py comments

--- server/database.py
+++ server/database.py
@@ -1,16 +1,18 @@
-#here we create our interface with the DB
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+#1. create our interface with the DB
+#===================================
 from config import Envs
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 
-#here we create our database connection 
-#DB details
+#2. create our database connection 
+#=================================
+#2.1. DB details
 DB_USER = Envs.DB_USER
-DB_PASSWORD = Envs.DB_PASSWORD
 DB_HOST = Envs.DB_HOST
 DB_PORT = Envs.DB_PORT
 DB_NAME = Envs.DB_NAME
+DB_PASSWORD = Envs.DB_PASSWORD
 
 DB_URL = f"mysql+mysqlconnector://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
