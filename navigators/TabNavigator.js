31c80edb5753e855396f1a4f72e38ded7f600d93
morenamt <onalepeloo@live.com>
2022-07-12 10:54:20 +0200

    added and configured the hamburger menu to the bottom tab navigator

--- navigators/TabNavigator.js
+++ navigators/TabNavigator.js
@@ -1,20 +1,20 @@
+//packages
 import { COLORS } from "./../constants";
-import {Ionicons, FontAwesome5 } from '@expo/vector-icons';
+import {Ionicons } from '@expo/vector-icons';
 import {createStackNavigator} from '@react-navigation/stack';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import { getFocusedRouteNameFromRoute } from '@react-navigation/native';
+import { getFocusedRouteNameFromRoute,useNavigation } from '@react-navigation/native';
 
 //screens
 import Home from './../screens/Home';
-import WishList from './../screens/WishList';
-import Details from './../screens/Details';
-import Signup from './../screens/Signup';
 import Login from './../screens/Login';
-import ResetPasswordRequest from './../screens/ResetPasswordRequest';
-import ResetPasswordInput from './../screens/ResetPasswordInput';
+import Signup from './../screens/Signup';
+import Details from './../screens/Details';
+import WishList from './../screens/WishList';
 import Verification from './../screens/OtpVerificationMsg';
+import ResetPasswordInput from './../screens/ResetPasswordInput';
+import ResetPasswordRequest from './../screens/ResetPasswordRequest';
 import OtpVerificationInput from './../screens/OtpVerificationInput';
-import TrendingNfts from './../screens/TrendingNfts';
 
 
 //navigators
@@ -39,14 +39,14 @@ const RootStack = () => {
            }}
            initialRouteName="Home"
          >
-               <Stack.Screen options={{headerTintColor: COLORS.primary}} name="Home" component={Home}/>
-               <Stack.Screen name="Login" component={Login}/>
-               <Stack.Screen name="Signup" component={Signup} />
-               <Stack.Screen name="ResetPasswordRequest" component={ResetPasswordRequest} />
-               <Stack.Screen name="ResetPasswordInput" component={ResetPasswordInput} />
-               <Stack.Screen name="Details" component={Details}/>
-               <Stack.Screen name="Verification" component={Verification}/>
-               <Stack.Screen name="OtpVerificationInput" component={OtpVerificationInput}/>
+            <Stack.Screen options={{headerTintColor: COLORS.primary}} name="Home" component={Home}/>
+            <Stack.Screen name="Login" component={Login}/>
+            <Stack.Screen name="Signup" component={Signup} />
+            <Stack.Screen name="ResetPasswordRequest" component={ResetPasswordRequest} />
+            <Stack.Screen name="ResetPasswordInput" component={ResetPasswordInput} />
+            <Stack.Screen name="Details" component={Details}/>
+            <Stack.Screen name="Verification" component={Verification}/>
+            <Stack.Screen name="OtpVerificationInput" component={OtpVerificationInput}/>
 
          </Stack.Navigator>
      )}
@@ -54,6 +54,7 @@ const RootStack = () => {
      
 
 const TabNavigator = () => {
+  const navigation = useNavigation();
   return (
     <Tab.Navigator
       screenOptions={{
@@ -76,15 +77,7 @@ const TabNavigator = () => {
           ),
         })}
       />
-      <Tab.Screen
-        name="Trending Nfts"
-        component={TrendingNfts}
-        options={{
-          tabBarIcon: ({color, size}) => (
-            <FontAwesome5 name="chart-bar" color={color} size={size} />
-          ),
-        }}
-      />
+
       <Tab.Screen
         name="WishList"
         component={WishList}
@@ -94,6 +87,24 @@ const TabNavigator = () => {
           tabBarIcon: ({color, size}) => (
             <Ionicons name="heart-outline" color={color} size={size} />
           ),
+        }}
+      />
+      <Tab.Screen
+        name="Menu"
+        component={Ionicons}
+        options={{
+          tabBarIcon: ({color, size}) => (
+            <Ionicons name="md-menu" color={color} size={size} />
+          ),
+        }}
+        listeners={{
+          tabPress: e => {
+            // Prevent default action
+            e.preventDefault();
+            
+            //open the drawer navigation
+            navigation.openDrawer();
+          },
         }}
       />
     </Tab.Navigator>
