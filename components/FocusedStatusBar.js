61c38b36cab292a1dc2a0cee0e2774c268300ada
morenamt <onalepeloo@live.com>
2022-07-14 10:37:47 +0200

    refactored the focussed status bar  js  file

--- components/FocusedStatusBar.js
+++ components/FocusedStatusBar.js
@@ -1,10 +1,11 @@
+//1. import all requred hook and component
 import { StatusBar } from 'react-native';
 import { useIsFocused } from '@react-navigation/core';
 
-//the props we are passing in are the background color and the height of the status bar
+//pass background color and the height of the status bar as props
 const FocussedStatusBar = (props) => {
-    
-    const isFocused = useIsFocused(); //gives us the information if the statusbar is focused or not
+
+    const isFocused = useIsFocused(); //is statusbar is focused or not
 
   return isFocused ? <StatusBar  animated={true} {...props}/> : null;
 }
