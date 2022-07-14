d2bb5d16f3365fa150651390c2fcf579bf70b03d
morenamt <onalepeloo@live.com>
2022-07-14 11:02:13 +0200

    refactored the sub info  component

--- components/SubInfo.js
+++ components/SubInfo.js
@@ -1,3 +1,5 @@
+//1. import all required constants and components
+//===============================================
 import { View, Image, Text } from "react-native";
 import { SIZES, FONTS, COLORS, SHADOWS, assets } from "../constants";
 
