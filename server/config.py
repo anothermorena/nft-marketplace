faa46176a113ffe5e347cf6f9e46f140eeb502fd
morenamt <onalepeloo@live.com>
2022-08-11 12:09:31 +0200

    refactored config.py to use heroku env vars instead of local vars

--- server/config.py
+++ server/config.py
@@ -4,35 +4,31 @@ import os
 import cloudinary
 import cloudinary.api
 import cloudinary.uploader
-from dotenv import load_dotenv
 from fastapi_mail import ConnectionConfig
 
-#2. load environment variables from .env
-#=======================================
-load_dotenv('./env')
 
-#3. create environment variables configuration class 
+#2. create environment variables configuration class 
 #===================================================
 class Envs:
-    DB_USER = os.getenv('DB_USER')
-    DB_PASSWORD = os.getenv('DB_PASSWORD')
-    DB_HOST = os.getenv('DB_HOST')
-    DB_PORT = int(os.getenv('DB_PORT'))
-    DB_NAME = os.getenv('DB_NAME')
-    MAIL_USERNAME = os.getenv('MAIL_USERNAME')
-    MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
-    MAIL_FROM = os.getenv('MAIL_FROM')
-    MAIL_PORT = int(os.getenv('MAIL_PORT'))
-    MAIL_SERVER = os.getenv('MAIL_SERVER')
-    MAIL_FROM_NAME = os.getenv('MAIN_FROM_NAME')
-    SECRET = os.getenv('SECRET')
-    ALGORITHM = os.getenv('ALGORITHM')
-    CLOUDINARY_NAME = os.getenv('CLOUDINARY_NAME')
-    CLOUDINARY_API_KEY = os.getenv('CLOUDINARY_API_KEY')
-    CLOUDINARY_API_SECRET = os.getenv('CLOUDINARY_API_SECRET')
+    DB_USER = os.environ.get('DB_USER')
+    DB_PASSWORD = os.environ.get('DB_PASSWORD')
+    DB_HOST = os.environ.get('DB_HOST')
+    DB_PORT = int(os.environ.get('DB_PORT'))
+    DB_NAME = os.environ.get('DB_NAME')
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    MAIL_FROM = os.environ.get('MAIL_FROM')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT'))
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_FROM_NAME = os.environ.get('MAIN_FROM_NAME')
+    SECRET = os.environ.get('SECRET')
+    ALGORITHM = os.environ.get('ALGORITHM')
+    CLOUDINARY_NAME = os.environ.get('CLOUDINARY_NAME')
+    CLOUDINARY_API_KEY = os.environ.get('CLOUDINARY_API_KEY')
+    CLOUDINARY_API_SECRET = os.environ.get('CLOUDINARY_API_SECRET')
     
     
-#4. setup the email server configuration
+#3. setup the email server configuration
 #=======================================
 email_conf = ConnectionConfig(
     MAIL_USERNAME=Envs.MAIL_USERNAME,
@@ -48,7 +44,7 @@ email_conf = ConnectionConfig(
 )
 
 
-#5. set up clouditionary configuration: return "https" URLs by setting secure=True  
+#4. set up clouditionary configuration: return "https" URLs by setting secure=True  
 #=================================================================================
 clouditionary_config = cloudinary.config(
             cloud_name= Envs.CLOUDINARY_NAME,
