eed69f26f09c16bf7566becaa11411b612d62c57
morenamt <onalepeloo@live.com>
2022-08-11 15:09:00 +0200

    added the live/production  server url to axios base url

--- api/axios.js
+++ api/axios.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 
 //2. set the url for the entire application
 //======================================
-const BASE_URL = 'https://6e9e-105-235-240-14.ngrok.io';
+const BASE_URL = 'https://another-nft-market-place.herokuapp.com/';
 
 export default axios.create({
     baseURL: BASE_URL
