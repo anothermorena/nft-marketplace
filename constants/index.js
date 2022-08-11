e4d775c890218dcbb413f2aca3748d45682ee75f
morenamt <onalepeloo@live.com>
2022-08-11 10:50:01 +0200

    prepared the server for deployment by deleting all client side code to reduce slug and added heroku procfile.Also added requirements to run the server

--- constants/index.js
+++
@@ -1,4 +0,0 @@
-import assets from "./assets";
-import { COLORS, SHADOWS, SIZES, FONTS } from "./theme";
-import Constants from 'expo-constants';
-export { assets, COLORS, SHADOWS, SIZES, FONTS, Constants };
--- contexts/CredentialsContext.js
+++
@@ -1,4 +0,0 @@
-//1. react context hook
-//=====================
-import { createContext } from 'react';
-export const CredentialsContext = createContext({ storedCredentials: {}, setStoredCredentials: () => {} });
