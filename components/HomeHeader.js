79cecacdab30a4c2747c05a26e464815bbca20d2
Otsogile Onalepelo <otsogile@weblogic.co.bw>
2023-08-13 14:51:51 +0200

    refactored the home header component to keep the code dry, removed inline styles and organized imports. Also added an avarta to use when the user is not logged in

--- components/HomeHeader.js
+++ components/HomeHeader.js
@@ -1,43 +1,84 @@
-//1. import all requred constants, hooks and components
-//=====================================================
-import { useContext } from 'react';
+import React, { useContext } from 'react';
 import { EvilIcons } from '@expo/vector-icons';
-import { COLORS, FONTS, SIZES} from "../constants";
-import {PageLogo} from './../components/StyledComponents';
-import { View, Text, TextInput, Pressable } from "react-native";
+import { View, Text, TextInput, Pressable, StyleSheet } from 'react-native';
+
+import { COLORS, FONTS, SIZES } from '../constants';
+import { PageLogo } from './../components/StyledComponents';
 import { CredentialsContext } from './../contexts/CredentialsContext';
 
-const HomeHeader = ({ onSearch, navigation,searchBarPlaceHolderText }) => {
+const HomeHeader = ({ onSearch, navigation, searchBarPlaceHolderText }) => {
+  const { storedCredentials } = useContext(CredentialsContext);
 
-  const { storedCredentials, setStoredCredentials } = useContext(CredentialsContext);
+  const userImageSource =
+    storedCredentials &&
+    storedCredentials.profileImage &&
+    storedCredentials.profileImage.trim() !== ''
+      ? { uri: storedCredentials.profileImage }
+      : require('./../assets/images/home-avatar.png');
 
   return (
-    <View style={{backgroundColor: COLORS.brand,padding: SIZES.font}}>
-      {storedCredentials === null ? ( 
-          <View style={{flexDirection: "row",justifyContent: "center",alignItems: "center"}}>
-              <Text style={{fontFamily: FONTS.bold,fontSize: SIZES.extraLarge,color: COLORS.white, textAlign:'center'}}>NFT Market Place</Text>
-          </View>
-      ) : (
-        <> 
-        <View style={{flexDirection: "row",justifyContent: "space-between",alignItems: "center"}}>
-          <Text style={{fontFamily: FONTS.bold,fontSize: SIZES.extraLarge,color: COLORS.white}}>NFT Market Place</Text>
-          <View style={{ width: 45, height: 45 }}>
-            <Pressable onPress={() => navigation.openDrawer()}>
-              <PageLogo resizeMode="cover" source={{uri: storedCredentials.profileImage}}  style={{width: "100%", height: "100%",borderRadius: 200}}/>
-            </Pressable>
-          </View>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerText}>NFT Market Place</Text>
+        <View style={styles.imageContainer}>
+          <Pressable onPress={() => navigation.openDrawer()}>
+            <PageLogo
+              resizeMode='cover'
+              source={userImageSource}
+              style={styles.userImage}
+            />
+          </Pressable>
         </View>
-        </>
-      )}
-  
-      <View style={{ marginTop: SIZES.font }}>
-        <View style={{width: "100%",borderRadius: SIZES.font,backgroundColor: COLORS.white,flexDirection: "row",alignItems: "center",paddingHorizontal: SIZES.font,paddingVertical: SIZES.small - 2}}>
-          <EvilIcons  name="search" size={35} color={COLORS.brand}  />
-          <TextInput placeholder={searchBarPlaceHolderText} style={{ flex: 1}} onChangeText={onSearch}/>
-        </View>
+      </View>
+      <View style={styles.searchContainer}>
+        <EvilIcons name='search' size={35} color={COLORS.brand} />
+        <TextInput
+          placeholder={searchBarPlaceHolderText}
+          style={styles.searchInput}
+          onChangeText={onSearch}
+        />
       </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: COLORS.brand,
+    padding: SIZES.font,
+  },
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  headerText: {
+    fontFamily: FONTS.bold,
+    fontSize: SIZES.extraLarge,
+    color: COLORS.white,
+  },
+  imageContainer: {
+    width: 45,
+    height: 45,
+  },
+  userImage: {
+    width: '100%',
+    height: '100%',
+    borderRadius: 200,
+  },
+  searchContainer: {
+    marginTop: SIZES.font,
+    width: '100%',
+    borderRadius: SIZES.font,
+    backgroundColor: COLORS.white,
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingHorizontal: SIZES.font,
+    paddingVertical: SIZES.small - 2,
+  },
+  searchInput: {
+    flex: 1,
+  },
+});
 
 export default HomeHeader;
